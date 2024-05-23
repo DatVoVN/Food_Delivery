412ac607134f0f2ac68dc9362780bf1daeacef6a
DatVoVN <dat246642@gmail.com>
2024-05-23 12:57:43 -0400

    Home

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>
+          Choose from a diverse menu featuring a delectable array of dishes
+          crafter with the finest ingredients and elevate your dining
+          experience, one delicious meal at the time
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
