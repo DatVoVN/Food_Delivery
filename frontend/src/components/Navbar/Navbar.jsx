3fe08d14a9b353b2c65ba5124d5e16e4ecb67336
DatVoVN <dat246642@gmail.com>
2024-05-30 12:12:56 -0400

    Website Resposive

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,32 +1,37 @@
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   const [menu, setMenu] = useState("HOME");
   return (
     <div className="navbar">
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li
+        <Link
+          to="/"
           onClick={() => setMenu("HOME")}
           className={menu === "HOME" ? "active" : ""}>
           HOME
-        </li>
-        <li
+        </Link>
+        <a
+          href="#explore-menu"
           onClick={() => setMenu("MENU")}
           className={menu === "MENU" ? "active" : ""}>
           MENU
-        </li>
-        <li
+        </a>
+        <a
+          href="#app-download"
           onClick={() => setMenu("MOBILE-APP")}
           className={menu === "MOBILE-APP" ? "active" : ""}>
           MOBILE-APP
-        </li>
-        <li
+        </a>
+        <a
+          href="#footer"
           onClick={() => setMenu("CONTACT-US")}
           className={menu === "CONTACT-US" ? "active" : ""}>
           CONTACT US
-        </li>
+        </a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
